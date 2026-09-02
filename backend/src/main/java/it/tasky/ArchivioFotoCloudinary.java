b41d2db67327f37f1f079ac20128888eddbf15ee
alessandro.nappa <alessandro01nappa@gmail.com>
2026-09-02 17:07:25 +0200

    Fix Cloudinary JSON mapper

--- backend/src/main/java/it/tasky/ArchivioFotoCloudinary.java
+++ backend/src/main/java/it/tasky/ArchivioFotoCloudinary.java
@@ -1,7 +1,7 @@
 package it.tasky;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URI;
